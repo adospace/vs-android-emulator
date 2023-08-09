768114ec99ca12f33ca9a933650c744f6ee44697
adospace <adospace@msn.com>
2023-08-10 00:37:54 +0200

    Fixing timing issues on slow PCs

---
+++ src/VsAndroidEmPackage.cs
@@ -0,0 +1,60 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VsAndroidEm
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideToolWindow(typeof(EmulatorWindow.Pane), Style = VsDockStyle.Tabbed, Window = WindowGuids.SolutionExplorer)]
+    [ProvideToolWindowVisibility(typeof(EmulatorWindow.Pane), VSConstants.UICONTEXT.SolutionHasSingleProject_string)]
+    [ProvideToolWindowVisibility(typeof(EmulatorWindow.Pane), VSConstants.UICONTEXT.SolutionHasMultipleProjects_string)]
+    [ProvideToolWindowVisibility(typeof(EmulatorWindow.Pane), VSConstants.UICONTEXT.NoSolution_string)]
+    [ProvideToolWindowVisibility(typeof(EmulatorWindow.Pane), VSConstants.UICONTEXT.EmptySolution_string)]
+
+    //[ProvideOptionPage(typeof(OptionsProvider.AdvancedOptions), "ChatGPT", "ChatGPT Helper Tool", 0, 0, true, new[] { "help", "chat", "gpt" })]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.VsAndroidEmString)]
+    public sealed class VsAndroidEmPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+
+            this.RegisterToolWindows();
+
+            //await JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            //IVsActivityLog log = (IVsActivityLog)(await GetServiceAsync(typeof(SVsActivityLog)));
+            //if (log == null) return;
+
+            //int hr = log.LogEntry((UInt32)__ACTIVITYLOG_ENTRYTYPE.ALE_INFORMATION,
+            //    this.ToString(),
+            //    string.Format(CultureInfo.CurrentCulture,
+            //    "Called for: {0}", this.ToString()));
+        }
+    }
+}
