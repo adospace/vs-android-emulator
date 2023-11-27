d321dbb757a55468fde2dab5032962372ef9a245
Adolfo Marinucci <adospace@msn.com>
2023-11-27 21:30:04 +0100

    Version 1.7

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,10 +9,10 @@ namespace VsAndroidEm
     {
         public const string Id = "VsAndroidEm.5dfcfa73-0d82-4b24-9c01-479eaa271e61";
         public const string Name = "VsAndroidEm";
-        public const string Description = @"This extension embeds the Android emulator directly inside a tool window of the IDE.";
+        public const string Description = @"This extension embeds the Android emulator directly inside a tool window of Visual Studio. It allows to switch between multiple running emulators, show/hide their toolbar, stop or shutdown them.";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "1.7";
         public const string Author = "Adolfo Marinucci";
-        public const string Tags = "";
+        public const string Tags = "android emulator";
     }
 }
